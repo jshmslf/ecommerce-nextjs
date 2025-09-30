b5d339aa97d8ca92212bc2b9ec2497b665758b72
jshmslf <joshuavf023@gmail.com>
2025-09-30 21:12:49 +0800

    first comms

---
+++ client/src/components/Searchbar.tsx
@@ -0,0 +1,12 @@
+import { Search } from "lucide-react"
+
+const Searchbar = () => {
+  return (
+    <div className="hidden sm:flex items-center gap-2 rounded-md ring-1 ring-gray-200 shadow-md">
+      <Search className="w-4 h-4 text-gray-500"/>
+      <input id="search" placeholder="Search..." type="text" className="text-sm outline-0" />
+    </div>
+  )
+}
+
+export default Searchbar
