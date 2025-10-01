b5279a2c3ec6de3c429dcda8b24d592bbe624d4c
jshmslf <joshuavf023@gmail.com>
2025-10-01 13:01:18 +0800

    steps@cart

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import Searchbar from './Searchbar'
-import { Bell, Home, ShoppingCart } from 'lucide-react'
+import { Bell, Home } from 'lucide-react'
+import ShoppingCartIcon from './ShoppingCartIcon'
 
 const Navbar = () => {
   return (
@@ -20,7 +21,7 @@ const Navbar = () => {
           <Home className='w-4 h-4 text-gray-600'/>
         </Link>
           <Bell className='w-4 h-4 text-gray-600'/>
-          <ShoppingCart className='w-4 h-4 text-gray-600' />
+          <ShoppingCartIcon/>
           <Link href={"/login"}>Sign in</Link>
       </div>
     </nav>
---
+++ client/src/components/ShippingForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShippingForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ShippingForm
